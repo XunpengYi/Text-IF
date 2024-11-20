e3c434a1abb8778c58c05ff3a45c07298c525f58
Xunpeng Yi <110455129+XunpengYi@users.noreply.github.com>
2024-11-20 21:17:47 +0800

    Add files via upload

--- scripts/losses.py
+++ scripts/losses.py
@@ -3,6 +3,33 @@ import torch.nn as nn
 import torch.nn.functional as F
 from math import exp
 
+class fusion_loss(nn.Module):
+    def __init__(self):
+        super(fusion_loss, self).__init__()
+        self.loss_func_ssim = L_SSIM(window_size=48)
+        self.loss_func_Grad = L_Grad()
+        self.loss_func_Max = L_Intensity()
+        self.loss_func_color = L_color()
+
+    def forward(self, image_visible, image_infrared, image_fused, max_ratio=4, ssim_vis_ratio=1, ssim_ir_ratio=1, ssim_ratio=1, color_ratio=12, text_ratio=10):
+        image_visible_gray = self.rgb2gray(image_visible)
+        image_infrared_gray = self.rgb2gray(image_infrared)
+        image_fused_gray = self.rgb2gray(image_fused)
+        loss_max = max_ratio * self.loss_func_Max(image_visible, image_infrared, image_fused)
+        loss_ssim = ssim_ratio * (ssim_vis_ratio * self.loss_func_ssim(image_visible, image_fused) + ssim_ir_ratio * self.loss_func_ssim(image_infrared_gray, image_fused_gray))
+        loss_color = color_ratio * self.loss_func_color(image_visible, image_fused)
+        loss_text = text_ratio * self.loss_func_Grad(image_visible_gray, image_infrared_gray, image_fused_gray)
+        total_loss = loss_max + loss_ssim + loss_color + loss_text
+        return total_loss, loss_ssim, loss_max, loss_color, loss_text
+
+    def rgb2gray(self, image):
+        b, c, h, w = image.size()
+        if c == 1:
+            return image
+        image_gray = 0.299 * image[:, 0, :, :] + 0.587 * image[:, 1, :, :] + 0.114 * image[:, 2, :, :]
+        image_gray = image_gray.unsqueeze(dim=1)
+        return image_gray
+
 class fusion_prompt_loss(nn.Module):
     def __init__(self):
         super(fusion_prompt_loss, self).__init__()
@@ -14,62 +41,41 @@ class fusion_prompt_loss(nn.Module):
         total_max_loss = 0
         total_color_loss = 0
         total_grad_loss = 0
-        for index in range(len(task)):
-            c = self.get_images(image_A, index)
-            if task[index] == "low_light":
-                loss, ssim_loss, max_loss, color_loss, grad_loss = self.fusion_loss(self.get_images(image_A, index), self.get_images(image_B, index), self.get_images(image_fused, index),
-                                                                                      max_ratio=8, consist_ratio=0)
-            elif task[index] == "over_exposure":
-                loss, ssim_loss, max_loss, color_loss, grad_loss = self.fusion_loss(self.get_images(image_A, index), self.get_images(image_B, index),
-                                                                                      self.get_images(image_fused, index), max_ratio=4, consist_ratio=4,
-                                                                                      ssim_ratio=0, ir_compose=1, text_ratio=2, max_mode="l2", consist_mode="l2")
-            elif task[index] == "ir_low_contrast":
-                loss, ssim_loss, max_loss, color_loss, grad_loss = self.fusion_loss(self.get_images(image_A, index), self.get_images(image_B, index),
-                                                                                      self.get_images(image_fused, index), max_ratio=8, consist_ratio=1)
-            elif task[index] == "ir_noise":
-                loss, ssim_loss, max_loss, color_loss, grad_loss = self.fusion_loss(self.get_images(image_A, index), self.get_images(image_B, index),
-                                                                                      self.get_images(image_fused, index), max_ratio=6, consist_ratio=4)
+
+        num_tasks = len(task)
+
+        for idx, task_type in enumerate(task):
+            img_A = self._get_image(image_A, idx)
+            img_B = self._get_image(image_B, idx)
+            img_fused = self._get_image(image_fused, idx)
+
+            if task_type == "low_light":
+                loss, ssim_loss, max_loss, color_loss, grad_loss = self.fusion_loss(img_A, img_B, img_fused,
+                                                                                    max_ratio=8, ssim_ratio=1, text_ratio=10)
+            elif task_type == "over_exposure":
+                loss, ssim_loss, max_loss, color_loss, grad_loss = self.fusion_loss(img_A, img_B, img_fused,
+                                                                                    max_ratio=4, ssim_ratio=0, text_ratio=2)
+            elif task_type == "ir_low_contrast":
+                loss, ssim_loss, max_loss, color_loss, grad_loss = self.fusion_loss(img_A, img_B, img_fused,
+                                                                                    max_ratio=8, ssim_ratio=1, text_ratio=10)
+            elif task_type == "ir_noise":
+                loss, ssim_loss, max_loss, color_loss, grad_loss = self.fusion_loss(img_A, img_B, img_fused,
+                                                                                    max_ratio=6, ssim_ratio=1, text_ratio=10)
+            else:
+                raise ValueError(f"Unknown task type: {task_type}")
+
             total_loss += loss
             total_ssim_loss += ssim_loss
             total_max_loss += max_loss
             total_color_loss += color_loss
             total_grad_loss += grad_loss
-        index = index + 1
-        return total_loss/index, total_ssim_loss/index, total_max_loss/index, total_color_loss/index, total_grad_loss/index
 
-    def get_images(self, images, index):
+        # Calculate the average for each loss component
+        return total_loss / num_tasks, total_ssim_loss / num_tasks, total_max_loss / num_tasks, total_color_loss / num_tasks, total_grad_loss / num_tasks,
+
+    @staticmethod
+    def _get_image(images, index):
         return images[index].unsqueeze(0)
-
-class fusion_loss(nn.Module):
-    def __init__(self):
-        super(fusion_loss, self).__init__()
-        self.loss_func_ssim = L_SSIM(window_size=48)
-        self.loss_func_Grad = GradientMaxLoss()
-        self.loss_func_Max = L_Intensity_Max_RGB()
-        # the consist loss is an aux loss, you can remove it as your needs
-        self.loss_func_Consist = L_Intensity_Consist()
-        self.loss_func_color = L_color()
-
-    def forward(self, image_visible, image_infrared, image_fused, max_ratio=4, consist_ratio=1, ssim_ir_ratio=1, ssim_ratio=1, ir_compose=1, color_ratio=12, text_ratio=10, max_mode="l1", consist_mode="l1"):
-        image_visible_gray = self.rgb2gray(image_visible)
-        image_infrared_gray = self.rgb2gray(image_infrared)
-        image_fused_gray = self.rgb2gray(image_fused)
-        loss_ssim = ssim_ratio * (self.loss_func_ssim(image_visible, image_fused) + ssim_ir_ratio * self.loss_func_ssim(image_infrared_gray, image_fused_gray))
-        loss_max = max_ratio * self.loss_func_Max(image_visible, image_infrared, image_fused, max_mode)
-        # the consist loss is an aux loss, you can remove it as your needs
-        loss_consist = consist_ratio * self.loss_func_Consist(image_visible_gray, image_infrared_gray, image_fused_gray, ir_compose, consist_mode)
-        loss_color = color_ratio * self.loss_func_color(image_visible, image_fused)
-        loss_text = text_ratio * self.loss_func_Grad(image_visible_gray, image_infrared_gray, image_fused_gray)
-        total_loss = loss_ssim + loss_max + loss_consist + loss_color + loss_text
-        return total_loss, loss_ssim, loss_max, loss_color, loss_text
-
-    def rgb2gray(self, image):
-        b, c, h, w = image.size()
-        if c == 1:
-            return image
-        image_gray = 0.299 * image[:, 0, :, :] + 0.587 * image[:, 1, :, :] + 0.114 * image[:, 2, :, :]
-        image_gray = image_gray.unsqueeze(dim=1)
-        return image_gray
 
 class L_color(nn.Module):
     def __init__(self):
@@ -102,38 +108,36 @@ class L_color(nn.Module):
         ycbcr_image = torch.stack((y, cb, cr), dim=1)
         return ycbcr_image
 
-class L_Intensity_Max_RGB(nn.Module):
+class L_Intensity(nn.Module):
     def __init__(self):
-        super(L_Intensity_Max_RGB, self).__init__()
+        super(L_Intensity, self).__init__()
 
-    def forward(self, image_visible, image_infrared, image_fused, max_mode="l1"):
+    def forward(self, image_visible, image_infrared, image_fused):
         gray_visible = torch.mean(image_visible, dim=1, keepdim=True)
         gray_infrared = torch.mean(image_infrared, dim=1, keepdim=True)
 
         mask = (gray_infrared > gray_visible).float()
 
         fused_image = mask * image_infrared + (1 - mask) * image_visible
-        if max_mode == "l1":
-            Loss_intensity = F.l1_loss(fused_image, image_fused)
-        else:
-            Loss_intensity = F.mse_loss(fused_image, image_fused)
+        Loss_intensity = F.l1_loss(fused_image, image_fused)
         return Loss_intensity
 
+# Use it only if you have a consistent modal preference
 class L_Intensity_Consist(nn.Module):
     def __init__(self):
         super(L_Intensity_Consist, self).__init__()
 
-    def forward(self, image_visible, image_infrared, image_fused, ir_compose, consist_mode="l1"):
+    def forward(self, image_visible, image_infrared, image_fused, vis_compose, ir_compose, consist_mode="l1"):
         if consist_mode == "l2":
-            Loss_intensity = (F.mse_loss(image_visible, image_fused) + ir_compose * F.mse_loss(image_infrared, image_fused))/2
+            Loss_intensity = (vis_compose * F.mse_loss(image_visible, image_fused) + ir_compose * F.mse_loss(image_infrared, image_fused))/2
         else:
-            Loss_intensity = (F.l1_loss(image_visible, image_fused) + ir_compose * F.l1_loss(image_infrared, image_fused))/2
+            Loss_intensity = (vis_compose * F.l1_loss(image_visible, image_fused) + ir_compose * F.l1_loss(image_infrared, image_fused))/2
         return Loss_intensity
 
-# use the GradientMaxLoss or L_Grad
-class GradientMaxLoss(nn.Module):
+# use the L_Grad_position or L_Grad
+class L_Grad_position(nn.Module):
     def __init__(self):
-        super(GradientMaxLoss, self).__init__()
+        super(L_Grad_position, self).__init__()
         self.sobel_x = nn.Parameter(torch.FloatTensor([[-1, 0, 1],
                                                        [-2, 0, 2],
                                                        [-1, 0, 1]]).view(1, 1, 3, 3), requires_grad=False).cuda()
@@ -275,54 +279,3 @@ class L_SSIM(torch.nn.Module):
             self.channel = channel
 
         return ssim(img1, img2, window=window, window_size=self.window_size, size_average=self.size_average)
-
-
-def structure_loss(img1, img2, window_size=11, window=None, size_average=True, full=False, val_range=None):
-    # Value range can be different from 255. Other common ranges are 1 (sigmoid) and 2 (tanh).
-    if val_range is None:
-        if torch.max(img1) > 128:
-            max_val = 255
-        else:
-            max_val = 1
-
-        if torch.min(img1) < -0.5:
-            min_val = -1
-        else:
-            min_val = 0
-        L = max_val - min_val
-    else:
-        L = val_range
-
-    padd = 0
-    (_, channel, height, width) = img1.size()
-    if window is None:
-        real_size = min(window_size, height, width)
-        window = create_window(real_size, channel=channel).to(img1.device)
-
-    mu1 = F.conv2d(img1, window, padding=padd, groups=channel)
-    mu2 = F.conv2d(img2, window, padding=padd, groups=channel)
-
-    mu1_sq = mu1.pow(2)
-    mu2_sq = mu2.pow(2)
-    mu1_mu2 = mu1 * mu2
-
-    sigma1 = F.conv2d(img1, window, padding=padd, groups=channel) - mu1
-    sigma2 = F.conv2d(img2, window, padding=padd, groups=channel) - mu2
-    sigma12 = F.conv2d(img1 * img2, window, padding=padd, groups=channel) - mu1_mu2
-    C2 = (0.03 * L) ** 2
-    loss=(2*sigma12+C2)/(2*sigma1*sigma2+C2)
-
-    if size_average:
-        ret = loss.mean()
-    else:
-        ret = loss.mean(1).mean(1).mean(1)
-
-    if full:
-        return 1 - ret
-    return ret
-
-def normalize_grad(gradient_orig):
-    grad_min = torch.min(gradient_orig)
-    grad_max = torch.max(gradient_orig)
-    grad_norm = torch.div((gradient_orig - grad_min), (grad_max - grad_min + 0.0001))
-    return grad_norm
